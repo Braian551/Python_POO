43c0065344c3aa46af7bef79fd3d20aa87273188
Braian <braianoquen@gmail.com>
2025-10-24 17:21:48 -0500

    primer commit

---
+++ usuario.py
@@ -0,0 +1,10 @@
+class Usuario:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def saludar(self):
+        return f"Hola, mi nombre es {self.nombre} y tengo {self.edad} años."
+
+Usuario1 = Usuario("Braian", 18)
+print(Usuario1.saludar())
